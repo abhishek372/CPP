6ead720df925ca3550afec660c41f4ce4297eef1
abhishek372 <abhishekkumar111109@gmail.com>
2022-12-22 03:22:32 +0530

    created variables

---
+++ 2_variables.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // variable a to store value 2
+    int a = 2; 
+    cout<<"Value of a: "<<a;
+    return 0;
+}
+
+// This is a single line comment
+/*
+This is 
+a multi-line
+comment.
+*/
